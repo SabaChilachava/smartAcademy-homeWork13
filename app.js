5dffb7979ff7aaa5aae4288833fdd82a9968d168
Saba Chilachava <120407795+SabaChilachava@users.noreply.github.com>
2023-04-06 13:52:16 +0400

    js

---
+++ app.js
@@ -0,0 +1,14 @@
+const name = `Saba`;
+const age = 24;
+const handsome = true;
+const iqLevel = undefined;
+const foo = null;
+
+
+const myComputerManufacturer = `Macbook Pro`;
+const myComputerMemory = `32GB`;
+const myComputerOS = `MacOS`;
+const computerBuyingYear = 2022;
+const currentYear = 2023;
+
+console.log(`my computer is` + ` ` + myComputerManufacturer + `, ` + `it runs on` + ` ` + myComputerOS + ` ` + `and I've been using it for` + ` ` + `${currentYear - computerBuyingYear}` + ` ` + `years.`);
